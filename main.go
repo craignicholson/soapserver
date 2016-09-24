d42de3f3ca326354ab22439b442c5d56208fe8af
Craig Nicholson <craig.nicholson@gmail.com>
2016-09-23 21:47:10 -0500

    Add main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+//  http://giantmachines.tumblr.com/post/49002286919/dealing-with-soap-xml-requests-in-golang
+
+package main
+
+import (
+	"fmt"
+"io/ioutil"
+	"log"
+				"net/http"
+)
+
+func main() {
+	http.HandleFunc("/soapserver", SoapServer)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+// SoapServer accepts and prints out the reponse body
+func SoapServer(w http.ResponseWriter, req *http.Request) {
+
+	defer req.Body.Close()
+
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n%s\n", b)
+	fmt.Printf("%s\n", req.Header)
+
+
+
+}
